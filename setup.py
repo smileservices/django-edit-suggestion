094fa12ca58c6123cdfd1ce8724cda529dcdd400
vladimir.gorea <vladimir.gorea@gmail.com>
2020-11-12 13:05:31 +0700

    update docs and increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-edit-suggestion',
-    version='1.21',
+    version='1.22',
     description='A django package for creating multiple users editable models',
     url='https://github.com/smileservices/django-edit-suggestion',
     author='Vladimir Gorea',
